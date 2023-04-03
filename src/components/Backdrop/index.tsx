4b72f32d2588dfdf7944c34b8666d7b3c0460a17
Lucas Overbey <lucasaoverbey@gmail.com>
2023-04-03 15:30:31 -0500

    fixed resume part

--- src/components/Backdrop/index.tsx
+++ src/components/Backdrop/index.tsx
@@ -7,7 +7,7 @@ const Backdrop = ({ children, onClick}) => {
     className="backdrop"
     onClick={onClick}
     initial={{ opacity: 0 }}
-    animate={{ opacity: 0.85 }}
+    animate={{ opacity: 1 }}
     exit={{ opacity: 0 }}
     >
       {children}
